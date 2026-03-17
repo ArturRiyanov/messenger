6ef71c47b340b549af6e4e9fbbdb68eecc94644a
Артур Риянов <49426563+ArturRiyanov@users.noreply.github.com>
2026-03-17 23:17:17 +0500

    HelloController

---
+++ src/test/java/com/example/riyanov/messenger/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riyanov.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
